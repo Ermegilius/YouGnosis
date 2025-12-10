3330a604be3bc3ab0d26a4a27983b3528a16efde
Vladimir Beliakov <ermegilius@gmail.com>
2025-12-10 11:21:54 +0200

    create fists test; exclude several lint rules for tests;  fix an import issue

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { AuthProvider } from "./context/AuthProvider.tsx";
 import { AppRouter } from "./router/router.tsx";
 import { ThemeProvider } from "./context/ThemeProvider.tsx";
-import { CookiesConsentProvider } from "./context/CookiesConsentContext.tsx";
+import { CookiesConsentProvider } from "./context/CookiesConsentProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
